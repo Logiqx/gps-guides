f7924fe6ac1f459b3b91998bde4d14dcbfe68f0d
Logiqx <software@mikeg.me.uk>
2022-04-08 11:46:19 +0100

    Move scripts to bin folder

--- sessions/bin/fit2csv.sh
+++ sessions/bin/fit2csv.sh
@@ -1,7 +1,9 @@
+BIN=$(dirname $0)
+
 for i in $(find . -name '*.fit')
 do
     if [ ! -e $(echo $i | sed 's/\.fit$/.csv/') ]
     then
-        java -jar FitCSVTool.jar "$i"
+        java -jar $BIN/FitCSVTool.jar "$i"
     fi
 done
