51f396d929ec1498da66f7d34737d2be06614803
Logiqx <software@mikeg.me.uk>
2022-04-08 09:45:27 +0100

    Ensure Garmin and COROS listings are sorted

--- sessions/contacts/list_coros.sh
+++ sessions/contacts/list_coros.sh
@@ -1,1 +1,1 @@
-find . -name '*.csv' -exec grep -H '^Data,0,file_id,.*manufacturer,"294",' {} \; | sed 's/:.*product_name,"/: /;s/",.*//'
+find . -name '*.csv' -exec grep -H '^Data,0,file_id,.*manufacturer,"294",' {} \; | sed 's/:.*product_name,"/: /;s/",.*//' | sort
--- sessions/contacts/list_garmin.sh
+++ sessions/contacts/list_garmin.sh
@@ -1,1 +1,1 @@
-find . -name '*.csv' -exec grep -H '^Data,0,file_id,.*manufacturer,"1",' {} \; | sed 's/:.*serial_number,"/: serial /;s/",.*garmin_product,"/ product /;s/",.*//'
+find . -name '*.csv' -exec grep -H '^Data,0,file_id,.*manufacturer,"1",' {} \; | sed 's/:.*serial_number,"/: serial /;s/",.*garmin_product,"/ product /;s/",.*//' | sort
