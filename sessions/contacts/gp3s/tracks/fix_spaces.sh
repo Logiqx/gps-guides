72ba8780a2085c855d6947000c9c949b9e881c83
Logiqx <software@mikeg.me.uk>
2022-04-08 10:53:54 +0100

    Fix spaces in filenames

---
+++ sessions/contacts/gp3s/tracks/fix_spaces.sh
@@ -0,0 +1,5 @@
+find . -type d -name '* *' -exec ./mv.sh {} \;
+find . -type d -name '* *' -exec ./mv.sh {} \;
+find . -type d -name '* *' -exec ./mv.sh {} \;
+
+find . -type f -name '* *' -exec ./mv.sh {} \;
